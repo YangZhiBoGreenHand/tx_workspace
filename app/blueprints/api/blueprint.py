51cedf33674948d37803289b5bd918268c5c3b23
yangzhibo <837805784@qq.com>
2023-10-11 11:09:17 +0800

    feat: add global exception handle

--- app/blueprints/api/blueprint.py
+++ app/blueprints/api/blueprint.py
@@ -1,4 +1,20 @@
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.base import app, db
 
 api = Blueprint("api", __name__)
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    db.session.rollback()
+    # 可以区分不同的异常，进行不同的处理
+    if isinstance(e, ValueError):
+        response = jsonify(message=str(e))
+        response.status_code = 400
+        return response
+
+    # 对于不可预知的错误，可以返回一个通用的500响应
+    response = jsonify(message="Internal Server Error")
+    response.status_code = 500
+    return response
